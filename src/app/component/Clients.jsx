049f339c77eea11b4ff52d28e34ef3cca30000ed
mumbaiops45 <mumbaiops45@gmail.com>
2026-02-16 15:46:52 +0530

    Updated project files

--- src/app/component/Clients.jsx
+++ src/app/component/Clients.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import ClientsCard from './ClientsCard'
+import ClientsCard from './ClientsCard';
 import "../styles/client.modal.css"
 
 const Clients = () => {
@@ -9,18 +9,8 @@ const Clients = () => {
         { number: "100000+", label: "Invoices Processed Per Month" },
         { number: "500+", label: "Completed Projects" },
         { number: "5000+", label: "Reconciliations Per Month" },
-        { number: "150+", label: "Team Size" },
-        
+        { number: "150+", label: "Team Size" }, 
     ];
-    // return (
-    //     <div className="stats-container-wrapper" style={{ top: "-55px" }}>
-    //         <div className="stats-container">
-    //             {stats.map((item, index) => (
-    //                 <ClientsCard key={index} number={item.number} label={item.label} />
-    //             ))}
-    //         </div>
-    //     </div>
-    // )
 
     return (
     <div className="stats-container-wrapper" style={{ top: "-55px" }}>
